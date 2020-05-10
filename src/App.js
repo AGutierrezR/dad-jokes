28fd00def586a403b5a720a3d74bb744439f4759
Andres Gutierrez <agutierrez.codes@gmail.com>
2020-05-10 02:51:19 +0200

    Agregar data
    Es una copia de lo que la API proporciona.
    
    Se usara para tener una idea de como organizarla

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import data from './data'
 
 const App = (props) => {
+  const [jokes, setJokes] = useState(data)
+
   return (
     <div className="App">
       <h1>{props.title}</h1>
+
+      {jokes.map(({ joke, id }) => joke)}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
