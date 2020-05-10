dd6dabfa9373528efd12ae2cd81ef16c365b6c2d
Andres Gutierrez <agutierrez.codes@gmail.com>
2020-05-10 02:21:45 +0200

    Agregar resetmini y title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const App = (props) => {
   return (
     <div className="App">
-
+      <h1>{props.title}</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './minireset.min.css'
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title="Random Dad Jokes"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
